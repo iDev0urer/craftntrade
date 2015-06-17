fe89a5c64f160bf468d9f3b48ba142067793e54b
Chris Watson <cawatson1993@gmail.com>
2015-06-17 14:22:37 -0600

    Ready for transfer to cloud9

--- app/controllers/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-  def sign_up_params
-    params.require(:user).permit(:first_name, :middle_initial, :last_name, :email, :username, :password, :password_confirmation, :mailing_list)
-  end
-
-end
